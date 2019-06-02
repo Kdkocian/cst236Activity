9d97b1a09549afb8a7e50f268d0bb7a696efae25
Kdkocian <krencent870@Gmail.com>
2019-06-02 18:27:51 -0500

    hulk smash code not work good!

--- SearchActivity/UserDataService.php
+++ SearchActivity/UserDataService.php
@@ -11,9 +11,16 @@ class UserDataService
     
     function findByFirstName($pattern)
     {
-        $database = new myfuncs();
-        
-        $db = $database->getConnect();
+        $servername = 'us-cdbr-iron-east-02.cleardb.net';
+        $username = 'bad2754a68df73';
+        $password = '008d5717';
+        $dbname = 'heroku_484ecd05b300db2';
+        //creates connection
+        $db =  mysqli_connect($servername, $username, $password, $dbname);
+        //tests the connection
+        if ($db->connect_error){
+            die("Connection failed: " . $db->connect_error);
+        }
         
         $sql = "SELECT * FROM users WHERE First_Name LIKE '%$pattern$'";
         
