cd656d5e55aedf78e21481633eecb8d98f10d9bc
Kdkocian <krencent870@Gmail.com>
2019-06-02 12:48:59 -0500

    trying to display search result

--- SearchActivity/searchHandler.php
+++ SearchActivity/searchHandler.php
@@ -5,13 +5,13 @@ $conn = $db->dbConnect();
 
 $searchresult = mysqli_real_escape_string($conn, $_GET['SearchBar']);
 
-$sql = "Select * from users where Last_Name LIKE '%a%'";
+$sql = "Select * from users where Last_Name LIKE '%$searchresult%'";
 
 $result = mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_array($result))
 {
-    echo "$row[ID], $row[First_Name], $row[Last_Name]"."<br>";    
+    echo $row['ID'], $row['First_Name'], $row['Last_Name']."<br>";    
 }
 echo "<a href='search.html'>Searchpage</a>";
 
