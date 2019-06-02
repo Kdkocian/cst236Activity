c76a473f35e54afb15515f1892bc436bb4b965da
Kdkocian <krencent870@Gmail.com>
2019-06-02 18:32:58 -0500

    brute force didn't work such is troubleshooting.

--- SearchActivity/UserDataService.php
+++ SearchActivity/UserDataService.php
@@ -11,36 +11,28 @@ class UserDataService
     
     function findByFirstName($pattern)
     {
-        $servername = 'us-cdbr-iron-east-02.cleardb.net';
-        $username = 'bad2754a68df73';
-        $password = '008d5717';
-        $dbname = 'heroku_484ecd05b300db2';
-        //creates connection
-        $db =  mysqli_connect($servername, $username, $password, $dbname);
-        //tests the connection
-        if ($db->connect_error){
-            die("Connection failed: " . $db->connect_error);
-        }
+        $database = new myfuncs();
+        
+        $conn = $database->getConnect();
         
         $sql = "SELECT * FROM users WHERE First_Name LIKE '%$pattern$'";
         
-        if($result = mysqli_query($db, $sql))
-        {
-           $index = 0;
+        $result = mysqli_query($conn, $sql);
+        
+           //$index = 0;
            $users = array();
            
            while ($row = $result->fetch_assoc())
            {
-               $users[$index] = array($row["ID"], $row["First_Name"], $row["Last_Name"]);
-               $index++;
+               $users[] = array($row["ID"], $row["First_Name"], $row["Last_Name"]);
+              // $index++;
            }
            $result->free();
-           $db->close();
+           $conn->close();
            
            if (count($users) > 0)
                return $users;
-           return NULL;
-        } 
+           return NULL;        
     }
 }
 ?>
