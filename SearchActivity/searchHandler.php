a2d2ab0fb2b88c33c2183296bd1ddbc8da46faf4
Kdkocian <krencent870@Gmail.com>
2019-06-03 16:43:11 -0500

    trying to find where the issue is

--- SearchActivity/searchHandler.php
+++ SearchActivity/searchHandler.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'autoLoader.php';
-echo "it made it here";
+
 $paTTern = $_POST["search"];
-
+echo "it made it here";
 $service = new UserBusinessService();
-
+echo "it made it this far";
 $users = $service->searchByFirstName($paTTern);
 
 $display = new _displayAllUsers($users);
