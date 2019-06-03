5c215ad383b53f0dedd7de00960c08728a8c05c0
Kdkocian <krencent870@Gmail.com>
2019-06-03 17:03:48 -0500

    trying to find where the issue is

--- SearchActivity/searchHandler.php
+++ SearchActivity/searchHandler.php
@@ -1,7 +1,10 @@
 <?php
 require_once 'autoLoader.php';
+require_once 'database.php';
+$db = new myfuncs();
+$conn = $db->getConnect();
 
-$paTTern = $_GET["search"];
+$paTTern = mysqli_real_escape_string($conn, $_GET['search']);
 
 $service = new UserBusinessService();
 
