a87c28b49f674b76c8f73d2e7c742c481d739350
Kdkocian <krencent870@Gmail.com>
2019-06-17 18:59:07 -0500

    created war game store

--- warGame/functions.php
+++ warGame/functions.php
@@ -10,7 +10,7 @@ function getAllproducts()
     
     while($row = mysqli_fetch_array($result))
     {
-        $products[] = array($row['weaponID'], $row['weapponName'], $row['weaponPrice']);
+        $products[] = array($row['weaponIDs'], $row['weaponName'], $row['weaponPrice']);
     }
     mysqli_close($conn);
     return $products;
@@ -20,13 +20,13 @@ function getAllIncart()
     $db = new myfuncs();
     $conn = $db->dbConnect();
     
-    $sql = "SELECT weaponID, weaponNAme, weaponPrice FROM cart";
+    $sql = "SELECT weaponIDs, weaponName, weaponPrice FROM cart";
     $weapon = array();
     $result = mysqli_query($conn, $sql);
     
     while($row = mysqli_fetch_array($result))
     {
-        $weapon[] = array($row['weaponID'], $row['waeponNAme'], $row['weaponPrice']);
+        $weapon[] = array($row['weaponIDs'], $row['waeponNAme'], $row['weaponPrice']);
     }
     mysqli_close($conn);
     return $weapon;
