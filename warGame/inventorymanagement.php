8a37f432938c34c62e002a429f929443c8b1505b
Kdkocian <krencent870@Gmail.com>
2019-06-17 18:54:25 -0500

    created war game store

--- warGame/inventorymanagement.php
+++ warGame/inventorymanagement.php
@@ -8,7 +8,7 @@ if(isset($_POST['addCart']))
     $weaponName = mysqli_real_escape_string($conn, $_POST['weaponName']);
     $weaponID = mysqli_real_escape_string($conn, $_POST['weaponID']);
     $weaponPrice = mysqli_real_escape_string($conn, $_POST['weaponPrice']);
-    $sql = "INSERT INTO cart(weaponID, weaponName, weaponPrice) VALUES('$weaponID', '$weaponName', '$weaponPrice')";
+    $sql = "INSERT INTO cart(weaponIDs, weaponName, weaponPrice) VALUES('$weaponID', '$weaponName', '$weaponPrice')";
     
     mysqli_query($conn, $sql);
     mysqli_close($conn);
